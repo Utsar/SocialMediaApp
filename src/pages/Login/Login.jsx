34df33f5c46a57e9122cf7876e661b94d5dd60eb
Kristian Utsar <utsaru@gmail.com>
2023-01-04 20:17:54 +0100

    login frame

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,20 +4,17 @@ const Login = () => {
   return (
     <div className="login">
       <div className="card">
-        <div className="right">
+        <div className="left">
           <h1>Hello World.</h1>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-            quisquam in distinctio dolorem, mollitia delectus reiciendis magni
-            modi! Rerum laboriosam totam, ducimus ut quaerat natus modi dolor
-            in, quis ipsa officia nostrum provident consequuntur, debitis vel
-            accusantium deleniti neque iste. Molestias deleniti distinctio
-            aperiam, impedit accusamus ab neque illo. Aliquid.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            assumenda nulla temporibus enim laborum, architecto officia
+            obcaecati nisi rerum numquam?
           </p>
           <span>Lorem, ipsum dolor.</span>
           <button>Register</button>
         </div>
-        <div className="left">
+        <div className="right">
           <h1>Login</h1>
           <form>
             <input type="text" placeholder="Username" />
